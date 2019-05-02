b2dd5d2c04ef77e3fab1b6e3bd62590cb603cdb9
ctobolski <christopher.tobolski@gmail.com>
2019-05-02 17:51:36 -0400

    Adding basic functionality

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+import { render, fireEvent, wait } from "react-testing-library";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("Can submit happy feedback", async () => {
+  const mockClient = {
+    submit: jest.fn(() => Promise.resolve())
+  };
+  const { getByText } = render(<App client={mockClient} />);
+  const happyButton = getByText(/happy/i);
+  fireEvent.click(happyButton);
+  fireEvent.click(getByText(/submit/i));
+  await wait(() => expect(mockClient.submit).toHaveBeenCalled());
+  expect(mockClient.submit).toHaveBeenCalledWith({
+    reaction: "happy",
+    thoughts: ""
+  });
+});
+
+it("Does not submit unless a reaction has been selected", () => {
+  const mockClient = {
+    submit: jest.fn()
+  };
+  const { getByText } = render(<App client={mockClient} />);
+  fireEvent.click(getByText(/submit/i));
+  expect(mockClient.submit).not.toHaveBeenCalled();
+  getByText(/Please let us know what you think before submitting/i);
+});
+
+it("Shows an error if user tries to submit without a reaction", () => {
+  const mockClient = {
+    submit: jest.fn()
+  };
+  const { getByText } = render(<App client={mockClient} />);
+  fireEvent.click(getByText(/submit/i));
+  expect(mockClient.submit).not.toHaveBeenCalled();
+});
+
+it("submits optional comments", () => {
+  const mockClient = {
+    submit: jest.fn(() => Promise.resolve())
+  };
+  const { getByText, getByLabelText } = render(<App client={mockClient} />);
+  const happyButton = getByText(/confused/i);
+  fireEvent.click(happyButton);
+  const thoughts = getByLabelText(/thoughts/i);
+  fireEvent.change(thoughts, {
+    target: { value: "What is the testing pyramid?" }
+  });
+
+  fireEvent.click(getByText(/submit/i));
+  expect(mockClient.submit).toHaveBeenCalledWith({
+    reaction: "confused",
+    thoughts: "What is the testing pyramid?"
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, FormEvent } from "react";
+import "./App.css";
 
-const App: React.FC = () => {
+interface Props {
+  client: {
+    submit: ({
+      reaction,
+      thoughts
+    }: {
+      reaction: string | null;
+      thoughts: string;
+    }) => void;
+  };
+}
+
+function App({ client }: Props) {
+  const [reaction, setReaction] = useState<string | null>(null);
+  const [thoughts, setThoughts] = useState<string>("");
+  const [error, setError] = useState(false);
+  function submit() {
+    if (reaction === null) {
+      setError(true);
+    } else {
+      client.submit({ reaction, thoughts });
+    }
+  }
+  function handleTextChange(event: FormEvent<HTMLInputElement>) {
+    setThoughts(event.currentTarget.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error && <ErrorMessage />}
+      <button onClick={() => setReaction("happy")}>Happy</button>
+      <button onClick={() => setReaction("confused")}>Confused</button>
+      <label htmlFor="thoughts-input">Thoughts</label>
+      <input
+        id="thoughts-input"
+        type="text"
+        value={thoughts}
+        onChange={handleTextChange}
+      />
+      <button onClick={submit}>Submit</button>
     </div>
   );
+}
+
+function ErrorMessage() {
+  return <div>Please let us know what you think before submitting</div>;
 }
 
 export default App;
